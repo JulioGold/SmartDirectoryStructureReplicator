ff8b5c0f5d96eb18b69e0ef52f6bd9cef57c7326
Julio Cesar Goldschmidt <jgoldschmidt@gvdasa.local>
2017-02-07 10:04:00 -0200

    Project created.

---
+++ src/SmartDirectoryStructureReplicator/SmartDirectoryStructureReplicator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SmartDirectoryStructureReplicator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Projetos";
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            DirectoryInfo[] directoryList = directoryInfo.GetDirectories("*", SearchOption.AllDirectories).Where(w =>
+                    !w.FullName.Contains("node_modules") &&
+                    !w.FullName.Contains("NugetPackages")
+                ).ToArray();
+
+            string rootPath = @"C:\Projetos\temp\Backup";
+
+            foreach (var item in directoryList)
+            {
+                Directory.CreateDirectory(item.FullName.Replace(path, rootPath));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
